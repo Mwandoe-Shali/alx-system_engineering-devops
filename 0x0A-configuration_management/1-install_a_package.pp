7fad25d01621509203a0e3f90a94d9df65a39696
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2024-02-10 19:37:54 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,7 +3,27 @@
 #   Install flask
 #   Version must be 2.1.0
 
-package { 'flask':
+# ensure python is installed
+package {'python3.8':
+  ensure => '3.8.10',
+}
+
+# ensure pip3 is installed
+package {'python3-pip':
+  ensure  => present,
+  # require => Package['python3.8'],
+}
+
+# install flask
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# install Werkzeug 2.1.1
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
