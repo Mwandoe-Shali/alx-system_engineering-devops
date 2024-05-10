73dd7b0c44232fc5ce68c5bb414b1e4a235e5f06
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-05-10 16:19:00 +0300

    For task 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+Queries the Reddit API for the number of subscribers of a given subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Retrieves the number of subscribers for the specified subreddit.
+
+    Returns:
+        int: The number of subscribers of the subreddit.
+             Returns 0 if the subreddit is invalid or inaccessible.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "My-Reddit-Scraper"}  # Custom User-Agent
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return (subscribers)
+    else:
+        return (0)
+
+
+if __name__ == "__main__":
+    print(number_of_subscribers("programming"))  # For testing purposes
